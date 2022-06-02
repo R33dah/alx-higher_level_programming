e24e468055ddd22cda524e2e4030ca3e19a7d425
r33edah <little.musahh@gmail.com>
2022-06-02 21:57:44 +0100

    program that imports the function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,10 +3,7 @@ from add_0 import add
 
 if __name__ == "__main__":
     """
-
     Prints the result of the addition between two numbers
-
     """
     a = 1
     b = 2
-    pint("{:d} + {:d} = {:d}".formate(a, b, add(a, b)))

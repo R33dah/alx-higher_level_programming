d5638c7997d9bb6c3fa09ae54d9bd80fa154f736
R33dah <99521096+R33dah@users.noreply.github.com>
2022-06-08 23:59:25 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def best_score(my_dict):
     if my_dict and len(my_dict):
         max = list(my_dict.keys())[0]
@@ -7,4 +6,4 @@ def best_score(my_dict):
             if my_dict[key] > my_dict[max]:
                 max = key
         return max
-    return None 
+    return None

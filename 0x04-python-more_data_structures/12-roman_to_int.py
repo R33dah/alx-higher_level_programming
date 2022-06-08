28d09fa44e34ebae4557b54f94a376a9d5ee03c9
r33edah <little.musahh@gmail.com>
2022-06-08 20:02:22 +0100

    function that  Fixes the logic of the program

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,44 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
+    """
+    A function that converts a Roman numeral to an integer.
+    """
+    if not roman_string:
         return 0
-
-    roman_letters = [
-        ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
-        ['X', 10], ['V', 5], ['I', 1]
-    ]
-    num = 0
-    last = 0
-
-    for letter in roman_string:
-        for elem in roman_letters:
-            if letter == elem[0]:
-                if last == 0 or last >= elem[1]:
-                    num += elem[1]
-                elif last < elem[1]:
-                    num += elem[1] - (last * 2)
-
-                last = elem[1]
-
-    return 
+    if not roman_string.isupper():
+        return 0
+    # Roman dictionary
+    Roman_dic = {
+            "I": 1,
+            "IV": 4,
+            "V": 5,
+            "IX": 9,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+            }
+    result = 0
+    temp = list(roman_string)
+    # Concat 4 and 9s
+    if len(temp) > 1:
+        idx = 0
+        for i in temp:
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'V':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'X':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            idx + 1
+    # Search in dic for correct number and add
+    for k, v in Roman_dic.items():
+        for index in temp:
+            if index == k:
+                result += v
+    return result

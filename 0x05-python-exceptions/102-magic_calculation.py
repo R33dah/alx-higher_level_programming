90e44cd871a17a7e5338529a3707753a1cbd4ceb
r33edah <little.musahh@gmail.com>
2022-06-23 00:10:12 +0100

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -12,7 +12,6 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except:
-            result = b + a
+                result = b + a
             break
     return (result)

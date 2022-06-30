5cdd3bb21061885812ef2ed1acdba8c24d12e82b
r33edah <little.musahh@gmail.com>
2022-06-30 20:51:02 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-""" This module is create with the add function
-The objetive of this function is to add numbers
+"""
+This module is composed by a function that adds two numbers
 """
 
 
 def add_integer(a, b=98):
-
-    """This is a function that adds 2 integers.
-    a and b must be integers or floats, otherwise raise a TypeError exception
-    with the message a must be an integer or b must be an integer
-    a and b must be first casted to integers if they are float
+    """ Function that adds two integer and/or float numbers
     Args:
-        a (int): The first parameter.
-        b (int): The second parameter, loaded to 98
+        a: first number
+        b: second number
     Returns:
-         An integer: the addition of a and b
+        The addition of the two given numbers
     Raises:
-        TypeError: if a or b don't be integers
-            exception with the message a must be an integer
-            or b must be an integer
+        TypeError: If a or b aren't integer and/or float numbers
     """
-    if type(a) is not int and type(a) is not float:
-        raise TypeError('a must be an integer')
-    if type(b) is not int and type(b) is not float:
-        raise TypeError('b must be an integer')
-    if a != a:
-        raise TypeError('a must be an integer')
-    if b != b:
-        raise TypeError('a must be an integer')
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
 
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
     return (a + b)

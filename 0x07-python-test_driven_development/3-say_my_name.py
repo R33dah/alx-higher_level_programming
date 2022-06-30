f485cb6ccb893e7dc0d033bdd2f046671a0cde42
r33edah <little.musahh@gmail.com>
2022-06-30 21:20:34 +0100

    3-say_my_name.

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-This module have a function that prints My name is <first name> <last name>
-prototype: def say_my_name(first_name, last_name=""):
-"""
+"""Defines a name-printing function."""
 
 
 def say_my_name(first_name, last_name=""):
-    """ function that prints My name is <first name> <last name>
-    first_name and last_name must be strings otherwise,
-    raise a TypeError exception with the message first_name must be a string
-    or last_name must be a string
-    Do not allowed to import any module
+    """Print a name.
     Args:
-        first name: first parameter
-        lasta name: second parameter
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
     Raises:
-        TypeError: first_name must be a string
-        TypeError: last_name must be a string
+        TypeError: If either of first_name or last_name are not strings.
     """
-    if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
-    if type(last_name) is not str:
-        raise TypeError('last_name must be a string')
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))

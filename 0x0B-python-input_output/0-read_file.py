a487f367a203295e1149417bdaadefd85a3a7d01
r33edah <little.musahh@gmail.com>
2022-07-05 20:32:14 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,15 @@
-[A[B#!/usr/bin/python3
+#!/usr/bin/python3
+""" Module that contains a function that reads from a file """
+
+
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
+    """ Function that reads from a file
+    Args:
+        filename: filename
+    Raises
+        Exception: when the file can be opened
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')

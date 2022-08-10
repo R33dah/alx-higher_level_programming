efdd89d57020c2c142a4786fbacb280eb9747a2d
R33dah <99521096+R33dah@users.noreply.github.com>
2022-08-10 21:20:20 +0100

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,4 +4,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;https://github.com/AnomalyCodes/alx-higher_level_programming/tree/main/0x0E-SQL_more_queries
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
